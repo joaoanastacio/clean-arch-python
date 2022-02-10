354bb83ff2f3d3cdf3b7c7b37ad0bc191d103690
Joao Victor Anastacio <profissional.anastacio@gmail.com>
2022-02-10 15:39:00 -0800

    refact: remove payment gateway, an gateway should be implemented in gateway layer

--- domain/entity/payment_gateway/payment_gateway.py
+++
@@ -1,9 +0,0 @@
-from typing import Protocol
-
-from domain.entity.transaction.transaction import Transaction
-
-class PaymentGateway(Protocol):
-
-    def pay(self, transaction: Transaction) -> None:
-        ...
-        
