955f77f06030ca9ebf3572c0b37dcecedd23cdd8
Joao Victor Anastacio <profissional.anastacio@gmail.com>
2022-02-16 15:51:47 -0800

    feature: finish singleton implementation to get mysql connection

--- infrastructure/database/mysql_connection.py
+++ infrastructure/database/mysql_connection.py
@@ -1,12 +1,28 @@
-from typing_extensions import Self
+import mysql.connector
+from mysql.connector import errorcode
 
+class MySqlConnection:
 
-
-class MySqlConnection(object):
-    
     __connection = None
 
-    def __new__(cls: type[Self]) -> Self:
-        if cls.__connection is None:
-            cls.__connection = super(MySqlConnection, cls).__new__(cls)
-        return cls.__connection
+    def connect(self):
+        if not self.__connection:
+            try:
+                config = {
+                    "user":"clean-arch-user",
+                    "password": "clean-arch-pwd",
+                    "host": "127.0.0.1",
+                    "database": "clean-arch",
+                    "raise_on_warnings": True
+                }
+                self.__connection =  mysql.connector.connect(**config)
+
+            except mysql.connector.Error as error:
+                if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                    print("Something is wrong with your user name or password")
+                elif error.errno == errorcode.ER_BAD_DB_ERROR:
+                    print("Database does not exist")
+                else:
+                    print(error)
+        else:
+            return self.__connection
